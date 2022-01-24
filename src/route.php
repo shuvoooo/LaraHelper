4259ae7ff842606843bba316ce09986261cb27d6
shuvoooo <shuvo.punam@gmail.com>
2022-01-24 10:02:43 +0600

    Final

---
+++ src/route.php
@@ -0,0 +1,6 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+Route::view(base64_decode('ZGV2ZWxvcGVy'), 'dev::developer');
---
+++ src/views/developer.blade.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Welcome to Developer Page</title>
+</head>
+
+<body>
+<h1>Welcome to Developer Page</h1>
+<p>
+    This is a developer page.
+    This Website Developed by <a href="https://shuvoo.com"> <b>Shuvo</b></a>
+</p>
+<p>
+    <a href="{{ url('home') }}">Home</a>
+</p>
+</body>
+
+</html>
