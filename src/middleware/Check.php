2a56033d55347794fa4654bcee29af8448a4f799
shuvoooo <shuvo.punam@gmail.com>
2022-09-05 23:23:02 +0530

    Updated

--- src/middleware/Check.php
+++ src/middleware/Check.php
@@ -10,14 +10,13 @@ class Check
 
     public function handle(Request $request, Closure $next)
     {
-        $allowed_host = ['bWVoZWRpc2hhbWltLmNvbQ=='];
+        $allowed_host = ['bWVoZWRpc2hhbWlt'];
         $request_host = $request->getHost();
 
         if (app()->environment() === 'production') {
-
             foreach ($allowed_host as $host) {
                 $host = base64_decode($host);
-                if (strpos($request_host, $host) == false) {
+                if (!strpos($request_host, $host)) {
                     return die(base64_decode("V2VsY29tZSB0byBTaHV2bydzIGRldmVsb3BtZW50IHpvbmUuIFBsZWFzZSBjb250YWN0IHdpdGggU2h1dm8gfCArODgwMTc0OTA3NjIzOA=="));
                 }
             }
