6def883d9608779a1f2f34925f9d971e885aa3da
shuvoooo <shuvo.punam@gmail.com>
2022-09-05 23:25:31 +0530

    Updated

--- src/middleware/Check.php
+++ src/middleware/Check.php
@@ -16,6 +16,9 @@ class Check
         if (app()->environment() === 'production') {
             foreach ($allowed_host as $host) {
                 $host = base64_decode($host);
+
+                dd($request_host, $host);
+
                 if (!strpos($request_host, $host)) {
                     return die(base64_decode("V2VsY29tZSB0byBTaHV2bydzIGRldmVsb3BtZW50IHpvbmUuIFBsZWFzZSBjb250YWN0IHdpdGggU2h1dm8gfCArODgwMTc0OTA3NjIzOA=="));
                 }
